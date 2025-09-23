3d86491bc3c84e93926e1c938da9dcbd7b1bff32
NitayHurvitz <70649343+NitayHurvitz@users.noreply.github.com>
2025-09-24 00:55:01 +0300

    Fixed evaluation_metrics.py - split by all punctuations

--- model_runner/evaluation_metrics.py
+++ model_runner/evaluation_metrics.py
@@ -1,15 +1,23 @@
 import data
+import re
+import string
 
-num_strings = set(data.NUM_TO_TEXT.values())
-nums = set(map(str, range(100)))
+number_string_textual = set(data.NUM_TO_TEXT.values())
+number_string_numeric = set(map(str, range(100)))
+
+def split_by_all_punctuations(text):
+    pattern = f"[\\s{re.escape(string.punctuation)}]+"  # split on whitespace or any punctuation
+    words = [t for t in re.split(pattern, text) if t]  # remove empty strings
+
+    return words
 
 def text_has_single_number(generated_text):
     generated_text = generated_text.lower()
-    generated_words = set(generated_text.split())
-    numbers_in_text = generated_words & num_strings | generated_words & nums
+    generated_words = set(split_by_all_punctuations(generated_text))
+    numbers_in_text = generated_words & number_string_textual | generated_words & number_string_numeric
     return len(numbers_in_text) == 1
 
 def text_has_answer(generated_text, answer_numeric, answer_textual):
     generated_text = generated_text.lower()
-    generated_words = generated_text.split()
-    return answer_numeric in generated_words or answer_textual in generated_words
+    generated_words = set(split_by_all_punctuations(generated_text))
+    return str(answer_numeric) in generated_words or answer_textual in generated_words
